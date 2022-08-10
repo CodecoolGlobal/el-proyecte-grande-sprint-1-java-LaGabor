fecd31fef4c16f59031f8575c9cf1d9be90c7f84
LaGabor <lagaborka@gmail.com>
2022-08-10 14:37:08 +0200

    Add @Controller annotation to the MainController Class.

--- src/main/java/com/fullteam/controller/MainController.java
+++ src/main/java/com/fullteam/controller/MainController.java
@@ -1,10 +1,11 @@
 package com.fullteam.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Controller
 public class MainController {
     @GetMapping("/")
     public  String MainPage() {
