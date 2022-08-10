46e25bb7967f34cd719e1364a406043e1233b70d
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-08-10 15:16:04 +0200

    Creates endpoint for game type's teams

--- src/main/java/com/fullteam/controller/MainController.java
+++ src/main/java/com/fullteam/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.fullteam.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -11,9 +13,10 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/team_list/{gameType}")
-    public  String teamPage() {
-        return "Duck";
+    @GetMapping("/teams/{gameType}")
+    public  String teamPage(@PathVariable String gameType, Model model) {
+        model.addAttribute("gameType", gameType);
+        return "teams";
     }
 
     @GetMapping("/game_register")
