0e8f2e33680c275c3ea9bf111427f93c1f3c6a46
Bence Fabian <bence.fabian@codecool.com>
2022-08-12 13:49:04 +0200

    Add type parameters
    Fixes fixes "unchecked" warnings

--- src/main/java/com/fullteam/FullTeamApplication.java
+++ src/main/java/com/fullteam/FullTeamApplication.java
@@ -1,5 +1,10 @@
 package com.fullteam;
 
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.fullteam.model.Game;
 import com.fullteam.model.GameType;
 import com.fullteam.model.Profile;
@@ -7,17 +12,9 @@ import com.fullteam.model.Team;
 import com.fullteam.service.GameCreator;
 import com.fullteam.service.ProfileCreator;
 import com.fullteam.service.TeamCreator;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FullTeamApplication {
@@ -40,8 +37,8 @@ public class FullTeamApplication {
 		Game pathFinder = gameCreator.creatGame("PathFinder",(byte)2,(byte)8, GameType.RPG,"Game for cool Ducks!");
 		Game dAndD = gameCreator.creatGame("Dungeons and Ducks",(byte)2,(byte)8, GameType.RPG,"Game for the coolest Ducks!");
 		Game magic = gameCreator.creatGame("Magic",(byte)2,(byte)4, GameType.RPG,"Card game for Ducks (with thumb)!");
-		Set boardGames = Stream.of(mars,monopoly,risk,cascadia).collect(Collectors.toSet());
-		Set onlineGames = Stream.of(marsOnline,diplomacy,dominion).collect(Collectors.toSet());
+		Set<Game> boardGames = Stream.of(mars,monopoly,risk,cascadia).collect(Collectors.toSet());
+		Set<Game> onlineGames = Stream.of(marsOnline,diplomacy,dominion).collect(Collectors.toSet());
 		Profile ducky = profileCreator.createProfile("Ducktor. Quackery Quack",LocalDate.of(2018,1,13),"I like bread and fish quack.",boardGames,onlineGames);
 		Team duckTales = teamCreator.createTeam("Duck Tales","Team for ducks who wears T-shirt BUT NO PANTS!",(byte)5,(byte)2,(byte)4,dominion,ducky);
 		Team wingsOfFreedom = teamCreator.createTeam("Wings of freedom","Fear our feathers.",(byte)5,(byte)2,(byte)4,dAndD,ducky);
