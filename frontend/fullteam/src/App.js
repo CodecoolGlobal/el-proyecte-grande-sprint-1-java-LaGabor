4ac9b79cc4c23e12f62c5cbbe16f11e5cc1ae1be
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-08-24 11:05:41 +0200

    Debugged double fetch at the startup

--- frontend/fullteam/src/App.js
+++ frontend/fullteam/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
     const [Game, setGame] = useState([]);
+    let effectRan = useRef(false);
 
-    const fetchUrl = (url, func, method = 'POST', body) => {
+    const fetchUrl = (url, func, body = {}, method = 'POST') => {
 
         const requestOption = {
             method: method,
@@ -12,24 +13,31 @@ function App() {
             body: JSON.stringify(body)
         }
 
-        fetch(url, requestOption)
-            .then(response => response.json())
-            .then(res => {
-                func(res)
-                console.log(res)
-            })
-            .catch(error => {
-                console.error('There was an error!', error)
-            });
+        if (effectRan.current === true){
+            fetch(url, requestOption)
+                .then(response => response.json())
+                .then(res => {
+                    func(res)
+                    console.log(res)
+                })
+                .catch(error => {
+                    console.error('There was an error!', error)
+                });
+
+        }
+        return () => {
+            effectRan.current = true
+        }
     }
 
     useEffect(() => {
-        fetchUrl('http://localhost:8080/game/list/board', setGame, {gameType: 'BOARD'})
+        return fetchUrl('http://localhost:8080/game/list/board', setGame, {gameType: 'BOARD'})
     }, [])
 
     return (
         <div className="App">
             <h1>FullTeam</h1>
+            {Game.map((game, index) => <p key={index}>{game})}</p>)}
         </div>
     );
 }
