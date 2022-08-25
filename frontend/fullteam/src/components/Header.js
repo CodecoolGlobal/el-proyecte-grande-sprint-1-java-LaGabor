84464e8ab3d924db711b30a4d32b7c427a209f9b
LaGabor <lagaborka@gmail.com>
2022-08-25 16:00:33 +0200

    Games link root added to the third icon.

--- frontend/fullteam/src/components/Header.js
+++ frontend/fullteam/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
                     <Link to="/" style={{ textDecoration: 'none' }}> <NavItems title="HOME" Icon={HomeIcon} /></Link>
                     <NavItems title="SEARCH" Icon={SearchIcon} />
                     <NavItems title="TRENDING" Icon={LightningBoltIcon} />
-                    <NavItems title="COLLECTIONS" Icon={CollectionIcon} />
+                    <Link to="games" style={{ textDecoration: 'none' }}><NavItems title="GAMES" Icon={CollectionIcon} /></Link>
                     <Link to="Login" style={{ textDecoration: 'none' }}><NavItems title="LOGIN" Icon={UserIcon} /></Link>
                 </div>
             </nav>
