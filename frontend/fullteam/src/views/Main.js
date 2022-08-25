a3084ed4ca976e303ec9e54942246732aecd7b5d
LaGabor <lagaborka@gmail.com>
2022-08-25 15:33:00 +0200

    Implement the Main.js and Main.css.
    Main Page with style.

--- frontend/fullteam/src/views/Main.js
+++ frontend/fullteam/src/views/Main.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Card from "../components/Card";
+import '../components/Main.css'
+import {Link} from "react-router-dom";
 const Main = () => {
     return(
         <div className="main">
-            <h2>This is the Main Page!</h2>
+            <div className="main-container">
+                <Link to='teams/rpg' style={{ textDecoration: 'none' }}><Card
+                    imgUrl="https://upload.wikimedia.org/wikipedia/commons/a/a1/RPG-28_grenade_launcher_at_Interpolitex-2016_01.jpg"
+                    title="RPG"
+                    description="Games for Man Of Culture"
+                /></Link>
+                <Link to='teams/online' style={{ textDecoration: 'none' }}><Card
+                    imgUrl="https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP"
+                    title="ONLINE GAMES"
+                    description="Dont Forget. Fifa and COD is for children!!!"
+                /></Link>
+                <Link to='teams/board' style={{ textDecoration: 'none' }}><Card
+                    imgUrl="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2020_25/3390425/board-games-kr-2x1-tease-200616.jpg"
+                    title="BOARD GAME"
+                    description="No its not skateboard."
+                /></Link>
+            </div>
         </div>
     );
 }
