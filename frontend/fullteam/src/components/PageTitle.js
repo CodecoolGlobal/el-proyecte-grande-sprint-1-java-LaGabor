ede2a0a1c6fd9e38ca266d3216124440ed750457
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-08-25 09:46:55 +0200

    Added multi page usage Title component

---
+++ frontend/fullteam/src/components/PageTitle.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageTitle = ({title}) => {
+
+    return (
+        <div className='pageTitleContainer'>
+            <h1 className='pageTitle'>{title}</h1>
+        </div>
+    )
+}
+
+export default PageTitle;
--- frontend/fullteam/src/views/Games.js
+++ frontend/fullteam/src/views/Games.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import GameList from '../components/GameList.js'
+import PageTitle from "../components/PageTitle";
 const Games = ({fetch}) => {
 
     return(
         <div className="gameRegister-container">
+            <PageTitle title='All Games Yet We Have.'/>
             <GameList fetch={fetch}/>
         </div>
     );
