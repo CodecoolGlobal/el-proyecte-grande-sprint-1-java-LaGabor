14d57efc6217c4fb1b202c753b0f18cec6413983
LaGabor <lagaborka@gmail.com>
2022-09-06 10:32:31 +0200

    Make Profile Class into an Entity.

--- backend/src/main/java/com/fullteam/model/Profile.java
+++ backend/src/main/java/com/fullteam/model/Profile.java
@@ -1,59 +1,43 @@
 package com.fullteam.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fullteam.model.types.GameType;
-import lombok.Builder;
-import lombok.Data;
-
+import lombok.*;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-@Data
+
+@Entity
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Profile {
 
-    private final String name;
-    private final LocalDate birthDate;
+    @Column(updatable = false)
+    private  String name;
+    @Column(updatable = false)
+    private  LocalDate birthDate;
     private final LocalDate registrationDate = LocalDate.now();
     @Builder.Default
     private  String description = "No description added yet.";
     @Builder.Default
+    @ManyToMany
     private Set<Game> ownedOnlineGames = new HashSet<>();
     @Builder.Default
+    @ManyToMany
     private Set<Game> ownedBoardGames = new HashSet<>();
-    private final int id;
 
-
-    public Profile(String name, LocalDate birthDate, String description, Set<Game> ownedOnlineGames, Set<Game> ownedBoardGames, int id) {
-        this.name = name;
-        this.birthDate = birthDate;
-        this.description = description;
-        this.ownedOnlineGames = ownedOnlineGames;
-        this.ownedBoardGames = ownedBoardGames;
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void addToOnlineGameList(Game game) {
-        if (game.getType() == GameType.ONLINE) {
-            ownedOnlineGames.add(game);
-        }
-        else {
-            throw new IllegalArgumentException("Game is not a computer game");
-        }
-    }
-
-    public void addToBoardGameList(Game game) {
-        if (game.getType() == GameType.BOARD) {
-            ownedBoardGames.add(game);
-        }
-        else {
-            throw new IllegalArgumentException("Game is not a board game");
-        }
-    }
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(updatable = false)
+    private  int id;
 
 }
