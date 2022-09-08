78582318d6b99f68bc4c9a137db7273aae01f91c
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-09-08 17:01:29 +0200

    Get team with fetch

--- frontend/fullteam/src/views/Team.js
+++ frontend/fullteam/src/views/Team.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import fetchUrl from "../fetch/fetch";
 const Team = () => {
+    const {id} = useParams()
+    const [Team, setTeam] = useState([]);
+    const url = 'http://localhost:8080/team/' + id;
+
+    useEffect( () => {
+        fetchUrl(url).then(response => setTeam(response))
+    }, [url])
+
+    console.log("In team: ", Team)
     return(
         <div className="team-container">
-            <h2>I am a Team Page!</h2>
+            <h2>{Team.teamName}</h2>
         </div>
     );
 }
