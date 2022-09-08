981cc1f45309827d498f342a43d7ebfed47c83a5
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-09-08 17:31:05 +0200

    Print to page some team details

--- frontend/fullteam/src/views/Team.js
+++ frontend/fullteam/src/views/Team.js
@@ -1,19 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import fetchUrl from "../fetch/fetch";
+import TeamCard from "../components/teamCard";
 const Team = () => {
-    const {id} = useParams()
-    const [Team, setTeam] = useState([]);
-    const url = 'http://localhost:8080/team/' + id;
-
-    useEffect( () => {
-        fetchUrl(url).then(response => setTeam(response))
-    }, [url])
-
-    console.log("In team: ", Team)
     return(
         <div className="team-container">
-            <h2>{Team.teamName}</h2>
+            <TeamCard />
         </div>
     );
 }
