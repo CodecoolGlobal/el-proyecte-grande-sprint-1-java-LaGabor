ae757ed02f12a8c7dddd2aedbf85a8ea73a758f2
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-09-08 17:01:59 +0200

    Send team for fetch

--- backend/src/main/java/com/fullteam/controller/GameController.java
+++ backend/src/main/java/com/fullteam/controller/GameController.java
@@ -1,12 +1,10 @@
 package com.fullteam.controller;
 
 import com.fullteam.controller.dto.GameDto;
-import com.fullteam.controller.dto.GameTypeDto;
 import com.fullteam.model.Game;
 import com.fullteam.model.types.GameType;
 import com.fullteam.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
--- backend/src/main/java/com/fullteam/controller/TeamController.java
+++ backend/src/main/java/com/fullteam/controller/TeamController.java
@@ -1,12 +1,25 @@
 package com.fullteam.controller;
 
-import org.springframework.stereotype.Controller;
+import com.fullteam.model.Team;
+import com.fullteam.service.TeamService;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class TeamController {
+
+    private final TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
 
     @GetMapping("/teams/{gameType}")
     public  String teamPage(@PathVariable String gameType, Model model) {
@@ -17,5 +30,10 @@ public class TeamController {
     @GetMapping("/team/register")
     public String teamRegister() {
         return "team_register";
+    }
+
+    @GetMapping("/team/{id}")
+    public Optional<Team> getTeamById(@PathVariable Long id) {
+        return teamService.findTeamById(id);
     }
 }
