1f91ba3bce417996338d53a33f9e18a479f14eb5
Kovacs Gergő András <kovacs.gergo.andras@gmail.com>
2022-09-21 11:10:22 +0200

    Add password and refactor name to username

--- backend/src/main/java/com/fullteam/data_sample/ProfileCreator.java
+++ backend/src/main/java/com/fullteam/data_sample/ProfileCreator.java
@@ -2,8 +2,10 @@ package com.fullteam.data_sample;
 
 import com.fullteam.model.Game;
 import com.fullteam.model.Profile;
+import com.fullteam.security.UserRole;
 import com.fullteam.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
@@ -14,13 +16,15 @@ public class ProfileCreator {
 
     private final ProfileService profileService;
     private final GameCreator gameCreator;
+    private final PasswordEncoder passwordEncoder;
     private Profile profile;
 
 
     @Autowired
-    public ProfileCreator(ProfileService profileService, GameCreator gameCreator) {
+    public ProfileCreator(ProfileService profileService, GameCreator gameCreator, PasswordEncoder passwordEncoder) {
         this.profileService = profileService;
         this.gameCreator = gameCreator;
+        this.passwordEncoder = passwordEncoder;
         initialize();
     }
 
@@ -30,11 +34,13 @@ public class ProfileCreator {
 
         Profile profile1 =
                 Profile.builder()
-                        .name("Ducktor Quackery Quack")
+                        .username("Ducktor Quackery Quack")
                         .birthDate(LocalDate.of(2018,1,13))
                         .description("I like bread and fish quack.")
                         .ownedBoardGames(Set.of(games.get(0),games.get(1)))
                         .ownedOnlineGames(Set.of(games.get(6),games.get(7)))
+                        .roles(UserRole.USER)
+                        .password(passwordEncoder.encode("kacsakacsa"))
                         .build();
         profileService.addProfile(profile1);
 
